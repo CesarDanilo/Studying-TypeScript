67b0c3bbb71eb807ac1152fdef6af325fce8cc2f
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-12-28 12:19:27 -0300

    tuplas

--- Object Types/Tuplas/index.js
+++ Object Types/Tuplas/index.js
@@ -1,1 +1,3 @@
 "use strict";
+const numberFive = [1, 2, 3, 4, 5];
+const nameAndAge = ["CESAR", 23];
---
+++ Object Types/Tuplas/index.ts
@@ -0,0 +1,7 @@
+type fiveNumbers = [number, number, number, number, number]
+
+const numberFive: fiveNumbers = [1, 2, 3, 4, 5]
+
+type nameandage = [string, number]
+
+const nameAndAge: nameandage = ["CESAR", 23]
