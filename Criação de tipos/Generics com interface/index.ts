55cf4cb0dbfe67c8dfd1b90e20ed56a1e35ac970
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-12-30 14:27:34 -0300

    teste

--- Criação de tipos/Generics com interface/index.ts
+++ Criação de tipos/Generics com interface/index.ts
@@ -1,24 +1,49 @@
-interface MyInterface<T, U> {
+// interface MyInterface<T, U> {
+//     nome: string
+//     idade: T,
+//     sexo: U
+// }
+
+// type car = MyInterface<number, boolean>
+// type person = MyInterface<number, string>
+
+
+// const myCar: car = {
+//     nome: 'Fusca',
+//     idade: 10,
+//     sexo: false
+// }
+
+// const myPerson: person = {
+//     nome: 'Cesar',
+//     idade: 23,
+//     sexo: 'M'
+// }
+
+// console.log(myCar)
+// console.log(myPerson)
+
+
+interface MyInterface<T, U, Z> {
     nome: string
     idade: T,
-    sexo: U
+    sexo: U,
+    marca: Z
 }
 
-type car = MyInterface<number, boolean>
-type person = MyInterface<number, string>
-
-
-const myCar: car = {
-    nome: 'Fusca',
-    idade: 10,
-    sexo: false
-}
+type person = MyInterface<number, string, boolean>
+type car = MyInterface<number, boolean, string>
 
 const myPerson: person = {
     nome: 'Cesar',
     idade: 23,
-    sexo: 'M'
+    sexo: 'M',
+    marca: false
 }
 
-console.log(myCar)
-console.log(myPerson)
+const mayCar: car = {
+    nome: 'Fusca',
+    idade: 10,
+    sexo: false,
+    marca: 'Volkswagen'
+}
